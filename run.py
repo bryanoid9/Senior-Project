b45e87352f8a7c7814722fd982a6cd1be8345c37
CastroIROCZ <124112134+CastroIROCZ@users.noreply.github.com>
2023-11-24 18:09:37 -0800

    Add files via upload
    updated

---
+++ run.py
@@ -0,0 +1,9 @@
+# run.py
+from appFactory import create_app, db
+
+app = create_app()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True)
